8131f81caecf53e4c7742384c800be6eebe4a79c
Guilherme <guilhermersouza34@gmail.com>
2024-04-08 18:13:22 -0300

    att

---
+++ dist/index.5564a631.js
@@ -0,0 +1,2 @@
+AOS.init();const e=new Date("Dec 12, 2027 19:00:00").getTime(),t=setInterval(function(){let n=e-new Date().getTime();document.getElementById("contador").innerHTML=`${Math.floor(n/864e5)}d ${Math.floor(n%864e5/36e5)}h ${Math.floor(n%36e5/6e4)}m ${Math.floor(n%6e4/1e3)}s`,n<0&&(clearInterval(t),document.getElementById("contador").innerHTML="Evento Expirado")},1e3);
+//# sourceMappingURL=index.5564a631.js.map
